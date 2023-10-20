e40ca8791309e2cf45615ddaba9fcb1745a07518
PramanaSaldin <saldin@wisc.edu>
2023-10-20 12:34:06 -0500

    fix orientation bug, added example simplices

--- simplicialComplex.py
+++ simplicialComplex.py
@@ -7,16 +7,16 @@ class SimplicialComplex:
     simplicialComplex: list[list[list[int]]] = []
 
     def __init__(self, dimension: int) -> None:
-        self.dimension = dimension
-        self.simplicialComplex = [[] for _ in range(dimension)]
+        self.dimension = dimension + 1
+        self.simplicialComplex = [[] for _ in range(self.dimension)]
 
     def add_simplex(self, simplex: list[int]) -> None:
-        self.simplicialComplex[len(simplex) - 1].append(simplex)
+        self.simplicialComplex[len(simplex) - 1].append(sorted(simplex))
 
     def get_k_simplices(self, k: int) -> list[list[int]]:
         return self.simplicialComplex[k]
 
-    # Gaussian elimination matrix rank
+    # Gaussian elimination matrix rank (doesn't work)
     # Source: https://cp-algorithms.com/linear_algebra/rank-matrix.html
     def __matrix_rank(self, M: np.ndarray) -> int:
         """
@@ -69,6 +69,7 @@ class SimplicialComplex:
     def boundary(self, k: int) -> np.ndarray:
         # boundary: k-simplices -> k-1-simplies
         # return a matrix from span{k-simplices} -> im(boundary)
+        # note that the k-simplices have k+1 entries
 
         basis_map = (
             dict()
@@ -82,9 +83,7 @@ class SimplicialComplex:
 
             for h in range(k + 1):
                 # consider the list without the hth element
-                boundary_simplex = tuple(simplex[:h] + simplex[(h + 1) :])
-
-                # sgn = self.perm_parity(boundary_simplex)
+                boundary_simplex = tuple(sorted(simplex[:h] + simplex[(h + 1) :]))
 
                 # boundary_simplex should now be sorted; we make this the canonical form
                 # and add it to the basis
@@ -92,16 +91,19 @@ class SimplicialComplex:
                     basis_map[boundary_simplex] = basis_n
                     basis_n += 1
 
+        # print(basis_map)
         M = np.zeros([len(k_simplices), len(basis_map)])
+
         for i, simplex in enumerate(k_simplices):
             row_vector: list[int] = [0] * len(basis_map)
-            # each simplex will have length k
 
             for h in range(k + 1):
                 # consider the list without the hth element
                 boundary_simplex = simplex[:h] + simplex[(h + 1) :]
+                # print(boundary_simplex)
 
                 sgn = self.perm_parity(boundary_simplex)
+                boundary_simplex.sort()
 
                 row_vector[basis_map[tuple(boundary_simplex)]] = sgn * (-1) ** h
 
@@ -110,10 +112,15 @@ class SimplicialComplex:
         return M
 
     def boundary_rank(self, k: int) -> int:
-        return self.__matrix_rank(self.boundary(k))
+        # return self.__matrix_rank(self.boundary(k))
+        return np.linalg.matrix_rank(self.boundary(k))
 
-    def betti_number(self, k: int) -> int:
-        return len(self.get_k_simplices(k)) - self.boundary_rank(k)
+    def betti_numbers(self) -> int:
+        b = [len(self.get_k_simplices(0)) - self.boundary_rank(1)]
 
-    def betti_numbers(self) -> list[int]:
-        return [self.betti_number(j) for j in range(self.dimension)]
+        for j in range(1, self.dimension-1): 
+
+            b.append(len(self.get_k_simplices(j)) - self.boundary_rank(j) - self.boundary_rank(j+1))
+            
+        b.append(len(self.get_k_simplices(self.dimension-1)) - self.boundary_rank(self.dimension-1))
+        return b
--- tester.py
+++ tester.py
@@ -1,7 +1,8 @@
+import numpy as np
 import SimplicialComplex
 
-if __name__ == "__main__":
-    simplex = SimplicialComplex.SimplicialComplex(4)
+class TwoSphere:
+    simplex = SimplicialComplex.SimplicialComplex(2)
 
     for j in range(4):
         simplex.add_simplex([j])
@@ -13,17 +14,75 @@ if __name__ == "__main__":
     simplex.add_simplex([1, 3])
     simplex.add_simplex([2, 3])
 
-    simplex.add_simplex([1, 2, 3])
+    simplex.add_simplex([2, 3, 1])
     simplex.add_simplex([2, 3, 4])
     simplex.add_simplex([1, 3, 4])
+    simplex.add_simplex([1, 2, 4])
 
-    simplex.add_simplex([1, 2, 3, 4])
+class Torus:
+    simplex = SimplicialComplex.SimplicialComplex(2)
 
+    for j in range(1,8):
+        simplex.add_simplex([j])
+
+    simplex.add_simplex([1,4])
+    simplex.add_simplex([1,3])
+
+    simplex.add_simplex([4,6])
+    simplex.add_simplex([4,5])
+    simplex.add_simplex([4,7])
+
+    simplex.add_simplex([5,7])
+    simplex.add_simplex([5,1])
+
+    simplex.add_simplex([3,2])
+    simplex.add_simplex([3,6])
+    simplex.add_simplex([3,5])
+    simplex.add_simplex([3,7])
+
+    simplex.add_simplex([6,7])
+    simplex.add_simplex([6,2])
+    simplex.add_simplex([6,5])
+    simplex.add_simplex([6,1])
+
+    simplex.add_simplex([7,2])
+
+    simplex.add_simplex([2,1])
+    simplex.add_simplex([2,4])
+    simplex.add_simplex([2,5])
+
+    simplex.add_simplex([1,4,3]) 
+    simplex.add_simplex([3,4,6])
+    simplex.add_simplex([4,6,7])
+    simplex.add_simplex([4,5,7])
+    simplex.add_simplex([1,5,7])
+    simplex.add_simplex([1,3,7])
+    simplex.add_simplex([3,5,6])
+    simplex.add_simplex([3,2,5])
+    simplex.add_simplex([2,4,5])
+    simplex.add_simplex([5,6,1])
+    simplex.add_simplex([2,1,4])
+    simplex.add_simplex([2,7,6])
+    simplex.add_simplex([2,3,7])
+    simplex.add_simplex([1,2,6])
+    
+
+
+
+if __name__ == "__main__":
+    two_sphere = TwoSphere()
     # print(f"the 0-simplices are {simplex.get_k_simplices(0)}")
     # print(f"the 1-simplices are {simplex.get_k_simplices(1)}")
 
-    i = 1
-    print(f"The rank of the {i}-boundary map is {simplex.boundary_rank(i)}")
-    i = 2
-    print(f"The rank of the {i}-boundary map is {simplex.boundary_rank(i)}")
-    print(f"The betti numbers are {simplex.betti_numbers()}")
+    print("2-SPHERE:")
+    for i in range(3):
+        print(f"The rank of the {i}-boundary map is {two_sphere.simplex.boundary_rank(i)}")
+        print(f"{i}-boundary map:\n{two_sphere.simplex.boundary(i)}")
+    
+    print(f"The betti numbers are {two_sphere.simplex.betti_numbers()}")
+
+
+    torus = Torus()
+    print("TORUS:")
+    print(torus.simplex.betti_numbers())
+
